2ff58273627a153f91d5a3a8de059a1f5f4ad2a4
yotsubo <513047.otsubo@gmail.com>
2024-04-25 15:14:04 +0900

    add: read_fd(), map_and_free()

--- get_next_line.c
+++ get_next_line.c
@@ -2,6 +2,38 @@
 
 #define BUFFER_SIZE 42
 
+static char	*map_and_free(char *res, char *for_free)
+{
+	free(for_free);
+	return (res);
+}
+
+static char	*read_fd(int fd, char *store)
+{
+	char	*res;
+	char	tmp[BUFFER_SIZE + 1];
+	int		read_res;
+
+	if (store)
+	{
+		res = ft_strjoin(res, store);
+		if (!res)
+			return (NULL);
+	}
+	read_res = read(fd, res, BUFFER_SIZE);
+	while (!ft_strchr(tmp, '\n') && read_size > 0)
+	{
+		read_res = read(fd, tmp, BUFFER_SIZE);
+		if (read_res == -1)
+			break ;
+		tmp[read_res] = '\0';
+		res = map_and_free(ft_strjoin(res, tmp), res);
+		if (!res)
+			return (NULL);
+	}
+	return (res);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*read_str;
