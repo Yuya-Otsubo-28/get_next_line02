62a3ef3f7b89b234cdb10910f91b5e10a89ba94a
yotsubo <513047.otsubo@gmail.com>
2024-04-30 17:20:05 +0900

    done2: read_fd()

--- get_next_line.c
+++ get_next_line.c
@@ -24,6 +24,8 @@ static char	*read_fd(int fd, char *store)
 	read_res = 1;
 	while (1)
 	{
+		if (ft_strchr(res, '\n') || read_res <= 0)
+			break ;
 		read_res = read(fd, tmp, BUFFER_SIZE);
 		if (read_res == -1)
 			break ;
@@ -31,47 +33,47 @@ static char	*read_fd(int fd, char *store)
 		res = map_and_free(ft_strjoin(res, tmp), res);
 		if (!res)
 			return (NULL);
-		if (ft_strchr(tmp, '\n') || read_res <= 0)
-			break ;
 	}
 	return (res);
 }
 
-char	*get_next_line(int fd)
-{
-	char		*read_str;
-	char		*line;
-	static char	*store;
+// char	*get_next_line(int fd)
+// {
+// 	char		*read_str;
+// 	char		*line;
+// 	static char	*store;
 
-	read_str = read_fd(fd, store);
-	if (!read_str)
-		return (NULL);
-	line = make_line(read_str);
-	if (!line)
-	{
-		free(read_str);
-		return (NULL);
-	}
-	store = make_store(read_str);
-	free(read_str);
-	if (!store)
-	{
-		free(line);
-		return (NULL);
-	}
-	return (line);
-}
+// 	read_str = read_fd(fd, store);
+// 	if (!read_str)
+// 		return (NULL);
+// 	line = make_line(read_str);
+// 	if (!line)
+// 	{
+// 		free(read_str);
+// 		return (NULL);
+// 	}
+// 	store = make_store(read_str);
+// 	free(read_str);
+// 	if (!store)
+// 	{
+// 		free(line);
+// 		return (NULL);
+// 	}
+// 	return (line);
+// }
 
 // int	main(void)
 // {
 // 	int		fd;
 // 	char	*res;
+// 	// char	*store = ft_strdup("yes!!\n");
 // 	char	*store = NULL;
 
 // 	fd = open("test.txt", O_RDONLY);
 // 	res = read_fd(fd, store);
 // 	printf("res: %s\n", res);
 // 	free(res);
+// 	// free(store);
 // 	close(fd);
 // 	return (0);
 // }
