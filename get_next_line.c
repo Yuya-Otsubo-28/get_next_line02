a38cd0b4dadc797a485b6ff48537144694eb65dc
yotsubo <513047.otsubo@gmail.com>
2024-05-04 18:55:34 +0900

    done without solve leaks and segvs

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 12
+#define BUFFER_SIZE 13
 
 static char	*map_and_free(char *res, char *for_free)
 {
@@ -15,7 +15,7 @@ static char	*read_fd(int fd, char *store)
 	int		read_res;
 
 	res = NULL;
-	if (store)
+	if (store && store[0])
 	{
 		res = ft_strjoin(res, store);
 		if (!res)
@@ -24,10 +24,10 @@ static char	*read_fd(int fd, char *store)
 	read_res = 1;
 	while (1)
 	{
-		if (ft_strchr(res, '\n') || read_res <= 0)
+		if (ft_strchr(res, '\n'))
 			break ;
 		read_res = read(fd, tmp, BUFFER_SIZE);
-		if (read_res == -1)
+		if (read_res <= 0)
 			break ;
 		tmp[read_res] = '\0';
 		res = map_and_free(ft_strjoin(res, tmp), res);
@@ -40,58 +40,81 @@ static char	*read_fd(int fd, char *store)
 static	char *make_line(char *read_str)
 {
 	size_t	len;
-	char	*res;
+	char	*line;
 
 	len = 0;
 	while (read_str[len] && read_str[len] != '\n')
 		len++;
-	res = ft_substr(read_str, 0, len);
-	if (!res)
+	line = ft_substr(read_str, 0, len);
+	if (!line)
 		return (NULL);
-	return (res);
+	return (line);
 }
 
-// char	*get_next_line(int fd)
-// {
-// 	char		*read_str;
-// 	char		*line;
-// 	static char	*store;
+static char	*make_store(char *read_str)
+{
+	char	*endl;
+	char	*store;
+	size_t	len;
 
-// 	read_str = read_fd(fd, store);
-// 	if (!read_str)
-// 		return (NULL);
-// 	line = make_line(read_str);
-// 	if (!line)
-// 	{
-// 		free(read_str);
-// 		return (NULL);
-// 	}
-// 	store = make_store(read_str);
-// 	free(read_str);
-// 	if (!store)
-// 	{
-// 		free(line);
-// 		return (NULL);
-// 	}
-// 	return (line);
-// }
+	endl = ft_strchr(read_str, '\n');
+	if (!endl)
+		return (ft_strdup(""));
+	endl++;
+	len = 0;
+	while (endl[len])
+		len++;
+	store = ft_substr(endl, 0, len);
+	if (!store)
+		return (NULL);
+	return (store);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*read_str;
+	char		*line;
+	static char	*store;
+
+	read_str = read_fd(fd, store);
+	if (!read_str)
+		return (NULL);
+	line = make_line(read_str);
+	if (!line)
+	{
+		free(read_str);
+		return (NULL);
+	}
+	store = make_store(read_str);
+	free(read_str);
+	if (!store)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
 
 int	main(void)
 {
 	int		fd;
 	char	*res;
-	char	*line;
-	// char	*store = ft_strdup("yes!!\n");
-	char	*store = NULL;
+	int	i = 0;
 
 	fd = open("test.txt", O_RDONLY);
-	res = read_fd(fd, store);
-	printf(" res: %s\n", res);
-	line = make_line(res);
-	printf("line: %s\n", line);
-	free(res);
-	free(line);
-	// free(store);
+	printf("res: %s\n", get_next_line(fd));
+	printf("res: %s\n", get_next_line(fd));
+	printf("res: %s\n", get_next_line(fd));
+	printf("res: %s\n", get_next_line(fd));
+	printf("res: %s\n", get_next_line(fd));
+	// while (i++ < 4)
+	// {
+	// 	res = get_next_line(fd);
+	// 	printf("res: %s", res); fflush(stdout);
+	// 	if (!res)
+	// 		break ;
+	// 	free(res);
+	// }
 	close(fd);
 	return (0);
 }
